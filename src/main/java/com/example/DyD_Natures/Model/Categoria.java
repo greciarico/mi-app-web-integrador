36e4d641cd85703822816a8c7d7a02c36f322fd3
greciarico <91849935+greciarico@users.noreply.github.com>
2025-06-09 17:09:16 -0500

    Update Categoria.java

--- src/main/java/com/example/DyD_Natures/Model/Categoria.java
+++ src/main/java/com/example/DyD_Natures/Model/Categoria.java
@@ -14,11 +14,8 @@ public class Categoria {
     @Column(name = "nombre_categoria", length = 100, nullable = false)
     private String nombreCategoria;
 
-    // CAMBIO CLAVE AQUÍ: De Boolean a Byte
     @Column(name = "estado", nullable = false)
-    private Byte estado = 1; // 0=Inactivo, 1=Activo, 2=Eliminado Lógicamente
-
-    // Getters y setters
+    private Byte estado = 1;
 
     public Integer getIdCategoria() {
         return idCategoria;
@@ -36,11 +33,11 @@ public class Categoria {
         this.nombreCategoria = nombreCategoria;
     }
 
-    public Byte getEstado() { // Ahora devuelve Byte
+    public Byte getEstado() {
         return estado;
     }
 
-    public void setEstado(Byte estado) { // Ahora acepta Byte
+    public void setEstado(Byte estado) {
         this.estado = estado;
     }
 }
