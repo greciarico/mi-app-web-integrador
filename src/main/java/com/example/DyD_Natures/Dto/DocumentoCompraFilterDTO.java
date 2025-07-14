b213ae9097a9719a0972f8d726c560a471add3a0
greciarico <91849935+greciarico@users.noreply.github.com>
2025-07-14 16:47:00 -0500

    Create DocumentoCompraFilterDTO.java

---
+++ src/main/java/com/example/DyD_Natures/Dto/DocumentoCompraFilterDTO.java
@@ -0,0 +1,85 @@
+package com.example.DyD_Natures.Dto;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+public class DocumentoCompraFilterDTO {
+    private String tipoDocumento;
+    private String numDocumento;
+    private Integer idProveedor;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate fechaRegistroStart;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate fechaRegistroEnd;
+    private BigDecimal totalMin;
+    private BigDecimal totalMax;
+    private List<Byte> estados; // Para filtrar por estado (Activo=1, Inactivo=0)
+
+    // Getters y Setters
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(String tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getNumDocumento() {
+        return numDocumento;
+    }
+
+    public void setNumDocumento(String numDocumento) {
+        this.numDocumento = numDocumento;
+    }
+
+    public Integer getIdProveedor() {
+        return idProveedor;
+    }
+
+    public void setIdProveedor(Integer idProveedor) {
+        this.idProveedor = idProveedor;
+    }
+
+    public LocalDate getFechaRegistroStart() {
+        return fechaRegistroStart;
+    }
+
+    public void setFechaRegistroStart(LocalDate fechaRegistroStart) {
+        this.fechaRegistroStart = fechaRegistroStart;
+    }
+
+    public LocalDate getFechaRegistroEnd() {
+        return fechaRegistroEnd;
+    }
+
+    public void setFechaRegistroEnd(LocalDate fechaRegistroEnd) {
+        this.fechaRegistroEnd = fechaRegistroEnd;
+    }
+
+    public BigDecimal getTotalMin() {
+        return totalMin;
+    }
+
+    public void setTotalMin(BigDecimal totalMin) {
+        this.totalMin = totalMin;
+    }
+
+    public BigDecimal getTotalMax() {
+        return totalMax;
+    }
+
+    public void setTotalMax(BigDecimal totalMax) {
+        this.totalMax = totalMax;
+    }
+
+    public List<Byte> getEstados() {
+        return estados;
+    }
+
+    public void setEstados(List<Byte> estados) {
+        this.estados = estados;
+    }
+}
