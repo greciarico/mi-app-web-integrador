4f80db6150d9493751f89c243cd4e24cac51a79d
greciarico <91849935+greciarico@users.noreply.github.com>
2025-07-14 16:58:56 -0500

    Update UsuarioRepository.java

--- src/main/java/com/example/DyD_Natures/Repository/UsuarioRepository.java
+++ src/main/java/com/example/DyD_Natures/Repository/UsuarioRepository.java
@@ -5,13 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer>, JpaSpecificationExecutor<Usuario> {
     Optional<Usuario> findByDniAndEstadoIsTrue(String dni);
     List<Usuario> findByEstadoNot(Byte estado);
 
