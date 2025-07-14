27dfa8956b13f4eeda4a40ec81f413d7ca1a439c
greciarico <91849935+greciarico@users.noreply.github.com>
2025-07-14 16:47:29 -0500

    Create MermaFilterDTO.java

---
+++ src/main/java/com/example/DyD_Natures/Dto/MermaFilterDTO.java
@@ -0,0 +1,87 @@
+package com.example.DyD_Natures.Dto;
+
+import java.time.LocalDate;
+
+public class MermaFilterDTO {
+    private String nombreProducto; // Para buscar por nombre de producto
+    private String descripcionMerma; // Para buscar por descripción de la merma
+    private Integer idProducto;     // Para filtrar por un producto específico
+    private LocalDate fechaRegistroStart;
+    private LocalDate fechaRegistroEnd;
+    private Integer cantidadMin;
+    private Integer cantidadMax;
+
+    // Constructor vacío
+    public MermaFilterDTO() {
+    }
+
+    // Getters y Setters
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public String getDescripcionMerma() {
+        return descripcionMerma;
+    }
+
+    public void setDescripcionMerma(String descripcionMerma) {
+        this.descripcionMerma = descripcionMerma;
+    }
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public LocalDate getFechaRegistroStart() {
+        return fechaRegistroStart;
+    }
+
+    public void setFechaRegistroStart(LocalDate fechaRegistroStart) {
+        this.fechaRegistroStart = fechaRegistroStart;
+    }
+
+    public LocalDate getFechaRegistroEnd() {
+        return fechaRegistroEnd;
+    }
+
+    public void setFechaRegistroEnd(LocalDate fechaRegistroEnd) {
+        this.fechaRegistroEnd = fechaRegistroEnd;
+    }
+
+    public Integer getCantidadMin() {
+        return cantidadMin;
+    }
+
+    public void setCantidadMin(Integer cantidadMin) {
+        this.cantidadMin = cantidadMin;
+    }
+
+    public Integer getCantidadMax() {
+        return cantidadMax;
+    }
+
+    public void setCantidadMax(Integer cantidadMax) {
+        this.cantidadMax = cantidadMax;
+    }
+
+    @Override
+    public String toString() {
+        return "MermaFilterDTO{" +
+                "nombreProducto='" + nombreProducto + '\'' +
+                "descripcionMerma='" + descripcionMerma + '\'' +
+                ", idProducto=" + idProducto +
+                ", fechaRegistroStart=" + fechaRegistroStart +
+                ", fechaRegistroEnd=" + fechaRegistroEnd +
+                ", cantidadMin=" + cantidadMin +
+                ", cantidadMax=" + cantidadMax +
+                '}';
+    }
+}
