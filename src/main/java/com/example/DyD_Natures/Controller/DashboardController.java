162442931869ae925cf9e5c021f040dce33e0c7a
greciarico <91849935+greciarico@users.noreply.github.com>
2025-07-17 19:12:48 -0500

    Update DashboardController.java

--- src/main/java/com/example/DyD_Natures/Controller/DashboardController.java
+++ src/main/java/com/example/DyD_Natures/Controller/DashboardController.java
@@ -10,25 +10,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-@Controller // Este controlador sirve vistas (HTML)
+@Controller 
 public class DashboardController {
     @Autowired
     private UsuarioService usuarioService;
 
-    @GetMapping("/") // Mapea la ruta raíz de tu aplicación
+    @GetMapping("/") 
     public String home(HttpServletRequest request, Model model, Authentication authentication) {
-        // Verifica si hay un usuario autenticado
+        
         if (authentication != null && authentication.isAuthenticated()) {
-            // El nombre de usuario en el Authentication object será el DNI,
-            // ya que así lo configuraste en tu UsuarioDetailsService.
             String dniUsuarioLogueado = authentication.getName();
-
-            // Busca el usuario completo en la base de datos usando el DNI
             usuarioService.obtenerUsuarioPorDni(dniUsuarioLogueado).ifPresent(usuario -> {
                 model.addAttribute("currentUser", usuario);
             });
         }
         model.addAttribute("currentUri", request.getRequestURI());
-        return "dashboard";  // Retorna el nombre de tu plantilla Thymeleaf (layout.html)
+        return "dashboard"; 
     }
 }
