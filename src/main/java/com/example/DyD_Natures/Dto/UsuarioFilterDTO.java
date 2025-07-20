a84972b5169b8aff3d9972d888ff7788c472796a
greciarico <91849935+greciarico@users.noreply.github.com>
2025-07-20 00:36:12 -0500

    Update UsuarioFilterDTO.java

--- src/main/java/com/example/DyD_Natures/Dto/UsuarioFilterDTO.java
+++ src/main/java/com/example/DyD_Natures/Dto/UsuarioFilterDTO.java
@@ -1,11 +1,14 @@
 package com.example.DyD_Natures.Dto;
 
+import java.time.LocalDate;
 import java.util.List; // Importar List
 
 public class UsuarioFilterDTO {
     private String nombreApellidoDniCorreo; // Para búsqueda general en esos campos
     private List<Integer> idRoles; // Cambiado a List<Integer> para múltiples selecciones de rol
     private List<Integer> estados; // Cambiado a List<Integer> para múltiples selecciones de estado
+    private LocalDate fechaRegistroStart; // Nuevo campo
+    private LocalDate fechaRegistroEnd;   // Nuevo campo
 
     // Getters y Setters
     public String getNombreApellidoDniCorreo() {
@@ -32,12 +35,31 @@ public class UsuarioFilterDTO {
         this.estados = estados;
     }
 
+    // Nuevos Getters y Setters para fechas
+    public LocalDate getFechaRegistroStart() {
+        return fechaRegistroStart;
+    }
+
+    public void setFechaRegistroStart(LocalDate fechaRegistroStart) {
+        this.fechaRegistroStart = fechaRegistroStart;
+    }
+
+    public LocalDate getFechaRegistroEnd() {
+        return fechaRegistroEnd;
+    }
+
+    public void setFechaRegistroEnd(LocalDate fechaRegistroEnd) {
+        this.fechaRegistroEnd = fechaRegistroEnd;
+    }
+
     @Override
     public String toString() {
         return "UsuarioFilterDTO{" +
                 "nombreApellidoDniCorreo='" + nombreApellidoDniCorreo + '\'' +
                 ", idRoles=" + idRoles +
                 ", estados=" + estados +
+                ", fechaRegistroStart=" + fechaRegistroStart +
+                ", fechaRegistroEnd=" + fechaRegistroEnd +
                 '}';
     }
 }
