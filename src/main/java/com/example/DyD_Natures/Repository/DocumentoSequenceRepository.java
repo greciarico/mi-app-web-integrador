893efd6c186c330bad128ea428344b0e025c1862
greciarico <91849935+greciarico@users.noreply.github.com>
2025-07-21 16:24:21 -0500

    Update DocumentoSequenceRepository.java

--- src/main/java/com/example/DyD_Natures/Repository/DocumentoSequenceRepository.java
+++ src/main/java/com/example/DyD_Natures/Repository/DocumentoSequenceRepository.java
@@ -1,16 +1,15 @@
 package com.example.DyD_Natures.Repository;
 
-import com.example.DyD_Natures.Model.DocumentoSequence; // Necesitarás crear este Modelo
+import com.example.DyD_Natures.Model.DocumentoSequence; 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.Lock; // Para bloqueo pesimista
+import org.springframework.data.jpa.repository.Lock; 
 import jakarta.persistence.LockModeType;
 import java.util.Optional;
 
 @Repository
 public interface DocumentoSequenceRepository extends JpaRepository<DocumentoSequence, Integer> {
 
-    // Usa bloqueo pesimista para asegurar el incremento atómico de la secuencia
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<DocumentoSequence> findByTipoDocumento(String tipoDocumento);
 }
